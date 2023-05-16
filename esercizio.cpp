9cce32e55cfb5688d295410cc731058d9e7795f7
Pasti Francesco <francesco.pasti@idbverona>
2023-05-16 09:49:49 +0200

    creazione

---
+++ esercizio.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main (void){
+	
+	int n1 = 0, n2 = 0,n3 = 0, n4 = 0, n5 = 0, n6 = 0, n7 = 0, n8 = 0, n9 = 0, n10 = 0;
+	int max = 0;
+	cout<<"Inserire un numero: ";
+	cin>>n1;
+	if (max < n1){
+		max = n1;
+	}
+	cout<<"Inserire un numero: ";
+	cin>>n2;
+	if (max < n2){
+		max = n2;
+	}
+	cout<<"Inserire un numero: ";
+	cin>>n3;
+	if (max < n3){
+		max = n3;
+	}
+	cout<<"Inserire un numero: ";
+	cin>>n4;
+	if (max < n4){
+		max = n4;
+	}
+	cout<<"Inserire un numero: ";
+	cin>>n5;
+	if (max < n5){
+		max = n5;
+	}
+	cout<<"Inserire un numero: ";
+	cin>>n6;
+	if (max < n6){
+		max = n6;
+	}
+	cout<<"Inserire un numero: ";
+	cin>>n7;
+	if (max < n7){
+		max = n7;
+	}
+	cout<<"Inserire un numero: ";
+	cin>>n8;
+	if (max < n8){
+		max = n8;
+	}
+	cout<<"Inserire un numero: ";
+	cin>>n9;
+	if (max < n9){
+		max = n9;
+	}
+	cout<<"Inserire un numero: ";
+	cin>>n10;
+	if (max < n10){
+		max = n10;
+	}
+	
+	cout<<max;
+	
+}
